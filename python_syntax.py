58cebe0c9c8b09200605fada69cc97f3c5e6ab5b
jay <jay@kaalpanik.in>
2022-07-14 12:56:52 +0530

    pyton syntax

---
+++ python_syntax.py
@@ -0,0 +1,15 @@
+# This is a variable
+
+'''
+Comments will be skipped from execution
+Python ignores comments
+single line or multiline it will ignore both comments
+'''
+number = 23 #Number
+float_number = 1.0333 #Float
+string = "good" # Str example
+
+# if statement example below
+if string == "good":
+    print(string, " & ", number, ' & ', float_number) #Indetation example
+    print(type(string)) # identify Type
