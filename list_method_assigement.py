93fd232687a98eb094eac9e2247575d6bbc20651
Tripti9-9 <109270289+Tripti9-9@users.noreply.github.com>
2022-07-20 16:38:15 +0530

    Add files via upload

---
+++ list_method_assigement.py
@@ -0,0 +1,22 @@
+alist = ['Tripti', 'jay', 24, 2022, 30.1, 'Dhanaji', 'google']
+alist.insert(4, 'oops')
+
+print(alist)
+
+alist = ['Tripti', 'jay', 24, 2022, 30.1, 'Dhanaji', 'google']
+alist.pop(3)
+
+print(alist)
+
+alist = ['Tripti', 'jay', 24, 2022, 30.1, 'Dhanaji', 'google']
+alist.remove("google")
+
+print(alist)
+
+alist = ['Tripti', 'jay', 24, 2022, 30.1, 'Dhanaji', 'google']
+alist.append('element_appended' )
+
+print(alist)
+
+
+
---
+++ string_method_assigement.py
@@ -0,0 +1,26 @@
+txt = "im going to be the best python developer"
+
+x = txt.capitalize()
+
+print (x)
+
+txt = "IM going to be best python developer"
+
+x = txt.lower()
+
+print(x)
+
+txt = "Im going to be the best python developer"
+x = txt.replace("best", "great")
+
+print(x)
+
+txt = "Im going to be the best python developer"
+x = txt.find("python")
+
+print(x)
+
+txt = "Im going to be the best python developer"
+x = txt.split()
+
+print(x)
