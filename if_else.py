c43dc71ef7e7101ec3348c4a22f1a388a3f87c93
jay <jchavan28@gmail.com>
2022-07-21 20:11:16 +0530

    adding if else and list tutorial

---
+++ if_else.py
@@ -0,0 +1,11 @@
+#  If something is true then do something
+# else if something is not true do something else
+
+door = "locked"
+
+if door == "open":
+    print("come in")
+elif door == "locked":
+    print("unlock the door")
+else:
+    print("open the door")
---
+++ list.py
@@ -0,0 +1,19 @@
+# List : are used to store multiple items
+
+a_simple_list = [ 1, 2, 3, 3, 3]
+a_bool_list = [ True, False, True, True]
+a_string_list = ["suresh", "mashesh", "digambar"]
+a_list = [1, True, "Mahesh"]
+
+#Get item from list based on index
+# print(a_list[1])
+
+#Change 1st item in list
+# a_list[1:3] = [False, "Suresh"]
+# print(a_list)
+
+# insert new item in list
+# a_list.insert(1, "Water")
+# print(a_list)
+
+#Assigment: practise and print all other methods of list
