6cf349671aa8e5fb7d195fecab330a57a6a4ee87
jay <jchavan28@gmail.com>
2022-07-21 19:42:44 +0530

    dictionary assignmnet

---
+++ assignment4_dict_tutorial.py
@@ -0,0 +1,12 @@
+#Dictonary is a datatype of key:value pairs
+
+#Methods & operations
+#Keys: print all keys
+#values: print all avalues
+#get value from key
+#update key : value
+#change : ["key_name"] : value
+#removea item from dict : pop()
+#Loop and print all keys in dict
+
+assign_dict = { } #add your own 5 key value pairs
